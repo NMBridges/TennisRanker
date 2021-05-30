788ab59ac2cd47feeaeaade165c3a6556bc2fd4e
Nolan <nolanmbridges@gmail.com>
2021-05-30 14:51:39 -0700

    First push

---
+++ rank.py
@@ -0,0 +1,93 @@
+import time
+import csv
+import re
+from selenium import webdriver
+
+# Created by Nolan Bridges and Alex Koong
+# May 30, 2021
+
+# Creates a Chrome WebDriver from the local path of the Chrome WebDriver.
+#   - It will be in a different place on every computer and does not come pre-installed.
+#   - Download here (note your installed version of Chrome): https://sites.google.com/a/chromium.org/chromedriver/downloads
+#   - Extract the .exe from the .zip folder and place in a place you would like to keep it. It does not matter.
+#   - Copy the path to where the chromedriver is, and paste it in the spot below. Make sure to replace any \ with \\ for proper syntax.
+#        - e.g. "C:\\Users\\scrat\\OneDrive\\Documents\\Dev\\Dependencies\\chromedriver.exe"
+PATH = ""
+driver = webdriver.Chrome(PATH)
+
+# Opens up an automated Chrome browser at the link below.
+driver.get("https://www.universaltennis.com/rankings")
+
+# Finds and clicks on the 'High School' option to ensure those are the rankings that are scraped by the program
+hsButton = driver.find_element_by_xpath("//*[@id=\"myutr-app-wrapper\"]/div[3]/div/div[5]/div[2]/div/div/div[1]/div[1]/div/span[4]")
+hsButton.click()
+
+# Instantiates a blank list of names to be added onto as the program scrapes universaltennis.com
+names = []
+
+# Creates a new .csv file to write the data to
+with open('ranks.csv', 'w', newline='') as ranks:
+    # Creates a CSVWriter object that allows writing to a .csv file
+    ranksWriter = csv.writer(ranks, delimiter=',')
+
+    # Writes the proper headers to the .csv file
+    ranksWriter.writerow(["Rank", "Name", "Class"])
+
+    time.sleep(3)
+
+    # The number of pages to cycle through. 
+    # Each page contains 100 players.
+    #       Precondition: 1 < pages <= 25
+    pages = 5
+
+    # Cycles through a preset amount of pages, scraping the names of the players and adding them to the 'names' list.
+    for x in range(1, pages + 1):
+        # Finds and clicks on the dropdown element that selects the proper range of players
+        #       i.e. Clicks on '201-300' from the dropdown so that the 201st-300th ranked players show up
+        setOfHundred = driver.find_element_by_xpath(f"//*[@id=\"myutr-app-wrapper\"]/div[3]/div/div[5]/div[2]/div/select/option[{x}]")
+        setOfHundred.click()
+
+        time.sleep(4)
+
+        # Cycles through each name shown on the page and adds it to the 'names' list.
+        for i in range(1, 101):
+            playerName = driver.find_element_by_xpath(f"//*[@id=\"myutr-app-wrapper\"]/div[3]/div/div[5]/div[4]/div[{i}]/a/div/div/div[2]/div/span[1]").text
+            names.append(playerName)
+
+    # Once all the pages are scraped and the names are found, the program will now go to tennisrecruiting.net to find the age/class of players.
+    # First, we will open up the automated Chrome browser at the following link:
+    driver.get("https://www.tennisrecruiting.net/player.asp")
+
+    # Then the program will go through every name in 'names' and find their tennisrecruiting.net profile
+    for index, name in enumerate(names):
+        # Finds the search box element, inputs the player's name, and hits enter to search.
+        textbox = driver.find_element_by_xpath("//*[@id=\"searchBox\"]/table/tbody/tr[2]/td[1]/input")
+        textbox.send_keys(name)
+        textbox.send_keys(webdriver.common.keys.Keys.RETURN)
+
+        time.sleep(1)
+
+        # Determines whether or not searching/querying for the player lands directly on their page immediately
+        onPage = False
+        contents = driver.find_elements_by_xpath("//*[@id=\"CenterColumn\"]/h1")
+        if len(contents) > 0:
+            if(contents[0].text == "PLAYER RECORD"):
+                # If the webpage has this element (which is unique to player profiles), the program has determined that it is on the player's page
+                onPage = True
+
+        # If the program is not on a player page (i.e. there are multiple people with similar names), it will select the first result from the search results.
+        #       The first result is generally the best matching name and/or highest ranked player, so this works most of the time (it has not failed yet, but I can see it happening).
+        if not onPage:
+            link = driver.find_element_by_xpath("//*[@id=\"CenterColumn\"]/div[2]/table/tbody/tr[2]/td[1]/b/a")
+            link.click()
+            time.sleep(1)
+
+        # From the player's page, it will find their graduation year
+        graduationYear = re.search(r'[12]\d{3}', driver.find_element_by_xpath("//*[@id=\"CenterColumn\"]/table[1]/tbody/tr/td[2]/table/tbody/tr[3]/td[2]/div[3]").text).group(0)
+        
+        # Adds the player's rank, name, and class to the .csv file.
+        print(f"{index + 1}: {name}, class of {graduationYear}")
+        ranksWriter.writerow([f"{index + 1}", f"{name}", f"{graduationYear}"])
+
+# Once the process is complete, the automated Chrome browser will close and the .csv file will be ready to use.
+driver.close()
