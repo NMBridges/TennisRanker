90cccd12c756be3b5234331c45033fcf66a485b5
Nolan <nolanmbridges@gmail.com>
2021-06-14 18:16:15 -0700

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -1,7 +1,6 @@
 import csv
 
 players = []
-utrs = []
 
 with open('co2022.csv', 'w', newline='') as co2022:
     with open('utrranks.csv', newline='', encoding='utf8') as utrRanks:
@@ -13,8 +12,8 @@ with open('co2022.csv', 'w', newline='') as co2022:
                     for row2 in trReader:
                         if row1['names'].lower() == row2['name'].lower():
                             players.append(row1['names'])
-                            utrs.append(row1['actual utr'])
-                            print(f"{len(players)}: {row1['names']}")
-
-for utr in utrs:
-    print(utr)
+                            out = f"{len(players)}: {row1['names'][:30]}"
+                            for i in range(30 - len(row1['names'][:30])):
+                                out += " "
+                            out += row1['actual utr']
+                            print(out)
